f7face3a3a150cf4ab451b2570092c15b2f2708d
Nicolas Martin <nicolas.martin.0986@gmail.com>
2014-06-29 23:02:54 -0400

    Fields on MainScene

---
+++ Field.h
@@ -0,0 +1,16 @@
+//
+//  Field.h
+//  SBNetromino
+//
+//  Created by nma on 2014-06-29.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Field : CCNode {
+    
+}
+
+@end
---
+++ Field.m
@@ -0,0 +1,14 @@
+//
+//  Field.m
+//  SBNetromino
+//
+//  Created by nma on 2014-06-29.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import "Field.h"
+
+
+@implementation Field
+
+@end
